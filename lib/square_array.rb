b408ee83c0eac76134f50ca8cc7154fc9d747956
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-20 19:28:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,5 @@
 
 def square_array(numbers)
-  numbers = [1,2,3,4,5]
   new_numbers= []
   numbers.length.times do |index|
     new_numbers.push(numbers[index]**2)
@@ -8,4 +7,4 @@ def square_array(numbers)
   puts new_numbers
 end
 
-square_array(numbers)
+square_array([1,2,3,4,5])
